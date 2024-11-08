1aa13be102769934f7abee4f38572fe518b27f40
Davide Paglieri <paglieridavide@gmail.com>
2024-11-08 18:25:40 +0000

    remove inform7 with aarch64

--- setup.sh
+++ setup.sh
@@ -15,57 +15,61 @@ case "${unameOut}" in
     *)          machine="UNKNOWN:${unameOut}"
 esac
 
+ARCH=$(uname -m)
+
 cd textworld/thirdparty/
 
-# Install command line Inform 7
-if [ ! -e I7_6M62_Linux_all.tar.gz ]; then
-    echo "Downloading Inform7 CLI"
-    curl -LO http://emshort.com/inform-app-archive/6M62/I7_6M62_Linux_all.tar.gz
-    if [ "${machine}" == 'Mac' ] && [ ! -e I7-6M62-OSX-Interim.dmg ]; then
-        echo "Downloading Inform7 for Mac"
-        curl -LO http://emshort.com/inform-app-archive/6M62/I7-6M62-OSX-Interim.dmg
+if [ "${ARCH}" != "aarch64" ]; then
+    # Install command line Inform 7
+    if [ ! -e I7_6M62_Linux_all.tar.gz ]; then
+        echo "Downloading Inform7 CLI"
+        curl -LO http://emshort.com/inform-app-archive/6M62/I7_6M62_Linux_all.tar.gz
+        if [ "${machine}" == 'Mac' ] && [ ! -e I7-6M62-OSX-Interim.dmg ]; then
+            echo "Downloading Inform7 for Mac"
+            curl -LO http://emshort.com/inform-app-archive/6M62/I7-6M62-OSX-Interim.dmg
+        fi
     fi
-fi
-if [ ! -d inform7-6M62 ]; then
-    tar xf I7_6M62_Linux_all.tar.gz
-fi
-(
-    echo "Installing Inform7 CLI"
-    cd inform7-6M62/
-    # Manually extract the files from the tarballs instead of calling install-inform7.sh.
-    # ./install-inform7.sh --prefix $PWD
-    tar xzf "inform7-common_6M62_all.tar.gz"
-    if [ "${machine}" != 'Mac' ]; then
-        ARCH=$(uname -m)
-        tar xzf "inform7-compilers_6M62_${ARCH}.tar.gz"
-        tar xzf "inform7-interpreters_6M62_${ARCH}.tar.gz"
+    if [ ! -d inform7-6M62 ]; then
+        tar xf I7_6M62_Linux_all.tar.gz
     fi
+    (
+        echo "Installing Inform7 CLI"
+        cd inform7-6M62/
+        # Manually extract the files from the tarballs instead of calling install-inform7.sh.
+        # ./install-inform7.sh --prefix $PWD
+        tar xzf "inform7-common_6M62_all.tar.gz"
+        if [ "${machine}" != 'Mac' ]; then
+            tar xzf "inform7-compilers_6M62_${ARCH}.tar.gz"
+            tar xzf "inform7-interpreters_6M62_${ARCH}.tar.gz"
+        fi
 
-    cd ..
-    rm -f inform7-6M62/share/inform7/Internal/I6T/Actions.i6t
-    cp inform7/share/inform7/Internal/I6T/Actions.i6t inform7-6M62/share/inform7/Internal/I6T/Actions.i6t
-)
+        cd ..
+        rm -f inform7-6M62/share/inform7/Internal/I6T/Actions.i6t
+        cp inform7/share/inform7/Internal/I6T/Actions.i6t inform7-6M62/share/inform7/Internal/I6T/Actions.i6t
+    )
 
-# Mount DMG if we're using a Mac
-if [ "${machine}" == 'Mac' ] && [ -e inform7-6M62 ]; then
-    echo "Mounting Inform for Mac"
-    hdiutil attach ./I7-6M62-OSX-Interim.dmg
+    # Mount DMG if we're using a Mac
+    if [ "${machine}" == 'Mac' ] && [ -e inform7-6M62 ]; then
+        echo "Mounting Inform for Mac"
+        hdiutil attach ./I7-6M62-OSX-Interim.dmg
 
-    echo "Copying Mac compiled inform files"
-    current_dir="$(pwd)"
-    cd /Volumes/Inform/Inform.app/Contents/MacOS
-    mkdir -p "$current_dir/inform7-6M62/share/inform7/Compilers/"
-    mkdir -p "$current_dir/inform7-6M62/share/inform7/Interpreters/"
-    cp cBlorb inform6 Inform intest ni "$current_dir/inform7-6M62/share/inform7/Compilers/"
-    cp ./git* "$current_dir/inform7-6M62/share/inform7/Interpreters/dumb-git"
-    cp ./glulxe* "$current_dir/inform7-6M62/share/inform7/Interpreters/dumb-glulxe"
+        echo "Copying Mac compiled inform files"
+        current_dir="$(pwd)"
+        cd /Volumes/Inform/Inform.app/Contents/MacOS
+        mkdir -p "$current_dir/inform7-6M62/share/inform7/Compilers/"
+        mkdir -p "$current_dir/inform7-6M62/share/inform7/Interpreters/"
+        cp cBlorb inform6 Inform intest ni "$current_dir/inform7-6M62/share/inform7/Compilers/"
+        cp ./git* "$current_dir/inform7-6M62/share/inform7/Interpreters/dumb-git"
+        cp ./glulxe* "$current_dir/inform7-6M62/share/inform7/Interpreters/dumb-glulxe"
 
-    cd "$current_dir"
+        cd "$current_dir"
 
-    echo "Unmounting Inform for Mac"
-    hdiutil detach /Volumes/Inform/
+        echo "Unmounting Inform for Mac"
+        hdiutil detach /Volumes/Inform/
+    fi
+else
+    echo "Skipping installation of Inform7 on aarch64 architecture."
 fi
-
 
 # Install modified fork of Glulx and build it - we have this one locally
 (
